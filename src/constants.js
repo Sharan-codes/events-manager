3e3e46f82edfa2bff65601d6c90df2b5e385c036
Sharan <sharan.s@juegostudio.net>
2020-07-29 19:01:33 +0530

    Replaced zeroes used for values by positive numbers.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-global.EVENT_INACTIVE = 0;
-global.EVENT_ACTIVE = 1;
-global.PRODUCT_BOUGHT = 2;
+global.EVENT_INACTIVE = 1;
+global.EVENT_ACTIVE = 2;
+global.PRODUCT_BOUGHT = 3;
 global.ADMIN = 1;
-global.USER = 0;
+global.USER = 2;
 global.TRUE = 1;
-global.FALSE = 0;
+global.FALSE = 2;
